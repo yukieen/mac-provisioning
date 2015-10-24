2c8fb3dd8f13864a26299eacc08f75dd21d41d17
yukieen <lpczclt@gmail.com>
2015-10-24 17:15:14 +0900

    add

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	
+brew install git
+	
+brew install caskroom/cask/brew-cask
+	
+brew cask install google-japanese-ime
+brew cask install google-chrome
+brew cask install slack
+brew cask install dockertoolbox
+brew cask install java
+brew cask install intellij-idea
+brew cask install atom
+
+brew install maven 
+brew install gradle
+
+brew cask install alfred
+
+docker-machine create --driver virtualbox default
